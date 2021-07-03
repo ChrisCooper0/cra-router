16b6c62e61586a469a9ebf80574614583b1df016
ChrisCooper0 <christopher.cooper@mail.com>
2021-07-03 14:15:12 +0100

    add ItemDetail page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from "./components/Nav";
 import About from "./components/About";
 import Shop from "./components/Shop";
 import Contact from "./components/Contact";
+import ItemDetail from "./components/ItemDetail";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -12,9 +13,9 @@ function App() {
         <Nav />
         <Switch>
           <Route path="/about" component={About}></Route>
-          <Route path="/shop" component={Shop}></Route>
+          <Route path="/shop" exact component={Shop}></Route>
+          <Route path="/shop/:id" component={ItemDetail}></Route>
           <Route path="/contact" component={Contact}></Route>
-          <Route path="/shop/:id"></Route>
         </Switch>
       </div>
     </Router>
--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,15 +1,34 @@
 import "../App.css";
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
-function Item() {
+function ItemDetail({ match }) {
+  const [item, setItem] = useState({});
+
+  useEffect(() => {
+    fetchItem();
+  }, []);
+
+  const fetchItem = async () => {
+    const res = await fetch(
+      `https://fakestoreapi.com/products/${match.params.id}`
+    );
+    const data = await res.json();
+    setItem(data);
+  };
+
   return (
     <div>
       <h1>Shop</h1>
       <div>
         <h1>Item</h1>
+        <h2 prop={item.id}>{item.title}</h2>
+        <p>{item.description}</p>
+        <img src={item.image} alt="product-image" width="200" height="250" />
+        <p>£{item.price}</p>
+        <button>BUY NOW</button>
       </div>
     </div>
   );
 }
 
-export default Item;
+export default ItemDetail;
